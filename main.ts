5262d6d9ecf9ebc9ea3343010f8dde9021f08ba2
AreebaNadeem567 <159957660+AreebaNadeem567@users.noreply.github.com>
2024-03-08 01:57:12 -0800

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,30 @@
+// Data types
+
+// Example 1 String 
+let fulName : string = "Areeba Nadeem";
+console.log(fulName);
+
+// Example 2 Number
+let age : number = 19;
+console.log(age);
+
+// Example 3 Boolean
+let isStudent : boolean = true;
+console.log(isStudent);
+
+// Example 4 Null
+let a : null = null;
+console.log(a);
+
+//Example  5 Undefined
+let b : undefined = undefined;
+console.log(b);
+
+// Example  6 BigInt
+let bigNumber : bigint = 567894328n;
+console.log(bigNumber);
+
+//Example 7 Symbol
+let uniqueSymbol = Symbol('Hello');
+console.log(uniqueSymbol);
+
